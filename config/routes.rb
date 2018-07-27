f7b3cb6c709944f97e2963d47fe23ee3fcf58f57
alex <oo.gaydar@gmail.com>
2018-07-27 17:13:47 +0300

    profile-9srv: Profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
   scope "(:locale)", locale: /en|ru/ do
+    get 'profile', to: 'profiles#show'
+    resources :profiles
     resources :orders
-    devise_for :users
+    # devise_for :users
+    devise_for :users, controllers: { sessions: 'users/sessions' }
     # get '/:locale' => 'welcome#index'
     root 'welcome#index'
     get 'welcome/index'
