21ca195938cc63d912dd521e05244834a9b48517
Alex <sashagajdarenko@gmail.com>
2019-04-12 20:24:49 +0300

    forma-23: 5 question

--- app/controllers/2seances_controller.rb
+++ app/controllers/2seances_controller.rb
@@ -20,23 +20,23 @@ class SeancesController < ApplicationController
 #    @seance = @user.seances.create(quest1: mas[0], quest2: mas[1], quest3: mas[2], quest4: mas[3], quest5: mas[4], quest6: mas[5], 
 #    quest7: mas[6], quest8: mas[7], quest9: mas[8], quest10: mas[9], )
 
-    @seances = Seance.first
+    #@seances = Seance.first
     #@seance_id = @seances.id
-    #@seance = Seance.first
-    #@seance_id = @seance.id
+    @seance = Seance.first
+    @seance_id = @seance.id
     @howmuch = '1'
     @one  = 0
     @two  = 0
     @three  = 0
     @four = 0
 # 1-й Вопрос - из поля quest1
-    @question = Question.find(@seances.quest1)    
+    @question = Question.find(@seance.quest1)    
     @test_question = @question.question
     @one_var = @question.answer1
     @two_var  = @question.answer2
     @three_var  = @question.answer3
     @four_var = @question.answer4
-#byebug
+byebug
     render "testing"
   
   end
@@ -77,84 +77,84 @@ class SeancesController < ApplicationController
     @two  = params[:two]
     @three  = params[:three]
     @four = params[:four]
-#byebug
+byebug
     if @howmuch == 1
-      @seances.be_quests = '1, '
-      if @one == '1'
-        @seances.answ1 = 1
-      elsif @two == '1'
-        @seances.answ1 = 2
-      elsif @three == '1'
-        @seances.answ1 = 3
-      elsif @four == '1'
-        @seances.answ1 = 4
+      @seance.be_quests = '1, '
+      if @one == 1
+        @seance.answ1 = 1
+      elsif @two == 1
+        @seance.answ1 = 2
+      elsif @three == 1
+        @seance.answ1 = 3
+      elsif @four == 1
+        @seance.answ1 = 4
       end
       # 2-й Вопрос - из поля quest2
-      @question = Question.find(@seances.quest2)
+      @question = Question.find(@seance.quest2)
     elsif @howmuch == 2
-      @seances.be_quests = @seances.be_quests + '2, '
-      if @one == '1'
-        @seances.answ2 = 1
-      elsif @two == '1'
-        @seances.answ2 = 2
-      elsif @three == '1'
-        @seances.answ2 = 3
-      elsif @four == '1'
-        @seances.answ2 = 4
+      @seance.be_quests = @seance.be_quests + '2, '
+      if @one == 1
+        @seance.answ2 = 1
+      elsif @two == 1
+        @seance.answ2 = 2
+      elsif @three == 1
+        @seance.answ2 = 3
+      elsif @four == 1
+        @seance.answ2 = 4
       end
       # 3-й Вопрос - из поля quest3
-      @question = Question.find(@seances.quest3)
+      @question = Question.find(@seance.quest3)
     elsif @howmuch == 3
-      @seances.be_quests = @seances.be_quests + '3, '
-      if @one == '1'
-        @seances.answ3 = 1
-      elsif @two == '1'
-        @seances.answ3 = 2
-      elsif @three == '1'
-        @seances.answ3 = 3
-      elsif @four == '1'
-        @seances.answ3 = 4
+      @seance.be_quests = @seance.be_quests + '3, '
+      if @one == 1
+        @seance.answ3 = 1
+      elsif @two == 1
+        @seance.answ3 = 2
+      elsif @three == 1
+        @seance.answ3 = 3
+      elsif @four == 1
+        @seance.answ3 = 4
       end
       # 4-й Вопрос - из поля quest4
-      @question = Question.find(@seances.quest4)
+      @question = Question.find(@seance.quest4)
     elsif @howmuch == 4
-      @seances.be_quests = @seances.be_quests + '4, '
-      if @one == '1'
-        @seances.answ4 = 1
-      elsif @two == '1'
-        @seances.answ4 = 2
-      elsif @three == '1'
-        @seances.answ4 = 3
-      elsif @four == '1'
-        @seances.answ4 = 4
+      @seance.be_quests = @seance.be_quests + '4, '
+      if @one == 1
+        @seance.answ4 = 1
+      elsif @two == 1
+        @seance.answ4 = 2
+      elsif @three == 1
+        @seance.answ4 = 3
+      elsif @four == 1
+        @seance.answ4 = 4
       end
       # 5-й Вопрос - из поля quest5
-      @question = Question.find(@seances.quest5)
+      @question = Question.find(@seance.quest5)
     elsif @howmuch == 5
-      @seances.be_quests = @seances.be_quests + '5; '
-      if @one == '1'
-        @seances.answ5 = 1
-      elsif @two == '1'
-        @seances.answ5 = 2
-      elsif @three == '1'
-        @seances.answ5 = 3
-      elsif @four == '1'
-        @seances.answ5 = 4
+      @seance.be_quests = @seance.be_quests + '5; '
+      if @one == 1
+        @seance.answ5 = 1
+      elsif @two == 1
+        @seance.answ5 = 2
+      elsif @three == 1
+        @seance.answ5 = 3
+      elsif @four == 1
+        @seance.answ5 = 4
       end
     end
-    @seances.save
+    @seance.save
     
-    @howmuch = @howmuch + 1
-#byebug
-    if @howmuch > 5 
-      render "show"
-    else
     @test_question = @question.question
     @one_var = @question.answer1
     @two_var  = @question.answer2
     @three_var  = @question.answer3
     @four_var = @question.answer4
     
+    @howmuch = @howmuch + 1
+byebug
+    if @howmuch > 5 
+      render "show"
+    else
       @howmuch = @howmuch.to_s
     end
   end
